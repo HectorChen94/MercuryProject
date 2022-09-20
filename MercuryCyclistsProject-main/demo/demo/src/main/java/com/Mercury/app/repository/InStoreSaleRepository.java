71ab813daa26cdc82baaa3f0c59c9faf94e14c17
Jhap-jhap <104487102+Jhap-jhap@users.noreply.github.com>
2022-09-20 22:41:33 +1000

    Update InStoreSaleRepository.java

--- MercuryCyclistsProject-main/demo/demo/src/main/java/com/Mercury/app/repository/InStoreSaleRepository.java
+++ MercuryCyclistsProject-main/demo/demo/src/main/java/com/Mercury/app/repository/InStoreSaleRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface InStoreSaleRepository extends JpaRepository<InStoreSale, String>
 {
-
+  Optional<InStoreSale> findInStoreSaleById(Long id);
 }
