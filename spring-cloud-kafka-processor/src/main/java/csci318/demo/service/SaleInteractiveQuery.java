49963e433f5cb6ef3e86e0a5e0ec85d6d70b0c3f
Jhap-jhap <104487102+Jhap-jhap@users.noreply.github.com>
2022-10-28 14:20:00 +1100

    Update SaleInteractiveQuery.java

--- spring-cloud-kafka-processor/src/main/java/csci318/demo/service/SaleInteractiveQuery.java
+++ spring-cloud-kafka-processor/src/main/java/csci318/demo/service/SaleInteractiveQuery.java
@@ -1,5 +1,6 @@
 package csci318.demo.service;
 
+import csci318.demo.model.Product;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -21,73 +20,32 @@ public class SaleInteractiveQuery
         this.interactiveQueryService = interactiveQueryService;
     }
 
-    public long getProductSales(String productName)
+    //Gets the total sales value of a product
+    public String getTotalSalesOfProduct(String productName)
     {
-        if (productStore().get(productName) != null){
-            return productStore().get(productName);
-        } else {
-            throw new NoSuchElementException();
-        }
-    }
-
-    public List<String> getProductList()
-    {
-        List<String> productList = new ArrayList<>();
-        KeyValueIterator<String, Long> all = productStore().all();
-        while (all.hasNext())
+        if (productStore.get(productName) == null)
         {
-            String next = all.next().key;
-            productList.add(next);
-        }
-        return productList;
-    }
-
-
-    public List<String> getTotalSalesOfProduct(String prodcutName)
-    {
-        if (productStore().get(productName) == null){
             throw new NoSuchElementException();
         } else {
-
-        }
-    }
-    /*
-    public List<String> getEquipmentListByBrand(String brandString) {
-        List<String> equipmentList = new ArrayList<>();
-        KeyValueIterator<String, Equipment> all = equipmentStore().all();
-        while (all.hasNext()) {
-            Equipment equipment = all.next().value;
-            String brand_name = equipment.getBrand();
-            String equipment_name = equipment.getEquipment();
-            if (brand_name.equals(brandString)){
-                equipmentList.add(equipment_name);
+            double totalValue = 0d;
+            KeyValueIterator<String, Product> all = productStore().all;
+            while (all.hasNext())
+            {
+                Product product = all.next().value;
+                totalValue += product.getPrice()*product.getProductSales();
             }
+            return productName+" total value: " + totalValue;
         }
-        return equipmentList;
     }
 
-
-    public List<String> getEquipmentList() {
-        List<String> equipmentList = new ArrayList<>();
-        KeyValueIterator<String, Equipment> all = equipmentStore().all();
-        while (all.hasNext()) {
-            String equipment_name = all.next().value.getEquipment();
-            equipmentList.add(equipment_name);
-        }
-
-        return equipmentList;
-    }
-    */
-
-    private ReadOnlyKeyValueStore<String, Long> productStore() {
-        return this.interactiveQueryService.getQueryableStore(ProductStreamProcessing.BRAND_STATE_STORE,
+    private ReadOnlyKeyValueStore<String, Product> productStore() {
+        return this.interactiveQueryService.getQueryableStore(ProductStreamProcessing.PRODUCT_STATE_STORE,
                 QueryableStoreTypes.keyValueStore());
     }
 
 
     private ReadOnlyKeyValueStore<String, Sale> saleStore() {
-        return this.interactiveQueryService.getQueryableStore(ProductStreamProcessing.EQUIPMENT_STATE_STORE,
+        return this.interactiveQueryService.getQueryableStore(ProductStreamProcessing.SALE_STATE_STORE,
                 QueryableStoreTypes.keyValueStore());
     }
 }
-
