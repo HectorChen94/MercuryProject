4c186c50d770efefe2131e0dad3460fa9b2275e2
Jhap-jhap <104487102+Jhap-jhap@users.noreply.github.com>
2022-10-28 14:07:46 +1100

    Update SaleQueryController.java

--- spring-cloud-kafka-processor/src/main/java/csci318/demo/controller/SaleQueryController.java
+++ spring-cloud-kafka-processor/src/main/java/csci318/demo/controller/SaleQueryController.java
@@ -14,32 +14,9 @@ public class SaleQueryController
     @Autowired
     SaleInteractiveQuery saleInteractiveQuery;
 
-    //Returns the quantity of sales of a particular product
-    @GetMapping("/product/{productName}/quantity")
-    long getProductSalesByName(@PathVariable String productName) {
-        return saleInteractiveQuery.getProductSales(productName);
-    }
-
-    @GetMapping("/product/all")
-    List<String> getAllProducts()
-    {
-        return saleInteractiveQuery.getProductList();
-    }
-
-    //Returns all of the sales of a particular product, not just the quantity
-    @GetMapping("/product/{productName}/sales")
-    List<String> getSalesByProduct(@PathVariable String productName)
-    {
-        return  saleInteractiveQuery.getSalesByProduct(productName);
-    }
-
-    @GetMapping("/sale/all-sales")
-    List<String> getAllSales() {
-        return  saleInteractiveQuery.getSalesList();
-    }
-
     @GetMapping("/product/{productName}/totalSalesValue")
-    List<String> getTotalSalesValueByProduct(@PathVariable String productName)
+    String getTotalSalesValueByProduct(@PathVariable String productName)
     { return saleInteractiveQuery.getTotalSalesOfProduct(productName); }
 
 }
+
