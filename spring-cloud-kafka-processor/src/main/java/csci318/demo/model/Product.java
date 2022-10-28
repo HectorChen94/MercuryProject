44726dc2404c835a54c4f97e86a6d54f81d48b4f
Jhap-jhap <104487102+Jhap-jhap@users.noreply.github.com>
2022-10-28 14:13:08 +1100

    Update Product.java

--- spring-cloud-kafka-processor/src/main/java/csci318/demo/model/Product.java
+++ spring-cloud-kafka-processor/src/main/java/csci318/demo/model/Product.java
@@ -19,39 +19,32 @@ public class Product
     @OneToMany(cascade = CascadeType.ALL)
     private List<Part> parts;
 
-
+    private int productSales;
 
     public Product() {}
 
-    public Product(Long productId, String productName, double price, String comment) {
-        this.productId = productId;
-        this.productName = productName;
-        this.price = price;
-        this.comment = comment;
-    }
-
-    public Product(Long productId, String productName, double price, String comment, List<Part> parts) {
+    public Product(Long productId, String productName, double price, String comment, List<Part> parts, int productSales) {
         this.productId = productId;
         this.productName = productName;
         this.price = price;
         this.comment = comment;
         this.parts = parts;
+        this.productSales = productSales;
     }
 
     public Long getProductId() {return productId;}
     public String getProductName() { return productName; }
     public double getPrice() { return price; }
     public String getComment() { return comment; }
-
     public List<Part> getParts() { return parts; }
+    public int getProductSales() { return productSales; }
 
     public void setProductId(Long productId) { this.productId = productId; }
     public void setProductName(String productName) { this.productName = productName; }
     public void setComment(String comment) { this.comment = comment; }
     public void setPrice(double price) { this.price = price; }
-
     public void setParts(List<Part> parts) { this.parts = parts; }
-
+    public void setProductSales(int productSales) { this.productSales = productSales; }
 
     @Override
     public String toString() {
